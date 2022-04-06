516deacbdd21d81ac325c9fa6923b69f7d827b7b
Franciane <franciane.penina@gmail.com>
2022-04-06 13:37:34 -0300

    layout para mobile aplicado

--- scripts.js
+++ scripts.js
@@ -1,6 +1,20 @@
 do{
     var selection = parseInt(window.prompt("Entre um número de 4 a 14! São aceitos apenas números pares", ""), 10);
 
-}while(isNaN(selection) || selection > 14 || selection < 4 || selection%2 !== 0);
+}while(isNaN(selection) || selection > 14 || selection < 4 || selection%2 !== 0){
+      let cont=0;
+        while(cont < selection){
+            var element = document.createElement("div");
+            element.classList.add("cartas");
+            document.querySelector(".corpo-cartas").appendChild(element);
+        cont++;
+}
+        
+}
+
+
+
+
+
 
 
