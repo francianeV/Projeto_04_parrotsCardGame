9f38a33f1d13ccb5511a1f8cdb80e2447ad26a62
Franciane <franciane.penina@gmail.com>
2022-04-11 01:59:57 -0300

    relogio

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 const cartas = ['unicornparrot','unicornparrot', 'tripletsparrot', 'tripletsparrot', 'revertitparrot', 'revertitparrot', 'metalparrot', 'metalparrot', 'fiestaparrot', 'fiestaparrot', 'explodyparrot', 'explodyparrot', 'bobrossparrot','bobrossparrot'];
-let cartasErradas = document.querySelectorAll(".cartaNaoOK");
+
+let cartasErradas = document.querySelectorAll(".cartaErrada");
 let numeroCartas = Number(prompt("Escolha o número de cartas que deseja iniciar o jogo. Obs: Apenas valores pares, entre 4 e 14."));
 let baralhoCortado = cartas.slice(0 , numeroCartas);
 let cartasEmbaralhadas = baralhoCortado.sort(comparador);
@@ -7,6 +8,24 @@ let primeiraCarta = null;
 let segundaCarta = null;
 let cartaVirada = null;
 let numeroJogadas = 0;
+
+let contCliques = 0
+let contadorSegundos = 0
+let contadorMinutos = 0 
+
+let interval = this.setInterval(function() {
+  if (contadorSegundos === 60) {
+    contadorMinutos++
+    contadorSegundos = 0
+  }
+  let minutos = document.querySelector('.relogio-minutos')
+  let segundos = document.querySelector('.relogio-segundos')
+
+  minutos.innerHTML = `⌚ ${contadorMinutos}m `
+  segundos.innerHTML = `${contadorSegundos}s`
+
+  contadorSegundos++
+},1000)
 
 function configurarJogo (){
     while (numeroCartas %2 !== 0 || numeroCartas < 4 || numeroCartas > 14){
@@ -29,12 +48,12 @@ function embaralharCartas(){
 
 function montarTabuleiro(cartasEmbaralhadas){
     for (i = 0; i < cartasEmbaralhadas.length; i++){  
-        document.querySelector(".tabuleiro").innerHTML+=`<div class='carta cartaNaoOk' onclick='virarCarta(this)'><img class='traseira-carta'src='imagens/front.png'/><img class='frente-carta escondido' src='imagens/${cartasEmbaralhadas[i]}.gif' /></div>`        
+        document.querySelector(".corpo-cartas").innerHTML+=`<div class='carta cartaErrada' onclick='virarCarta(this)'><img class='traseira-carta'src='imagens/front.png'/><img class='frente-carta escondido' src='imagens/${cartasEmbaralhadas[i]}.gif' /></div>`        
     }
 }    
 
 function virarCarta(cartaVirada){
-    if (cartaVirada.classList.contains("cartaNaoOk") === false){
+    if (cartaVirada.classList.contains("cartaErrada") === false){
         return
     }
     numeroJogadas ++; 
@@ -51,8 +70,8 @@ function virarCarta(cartaVirada){
 function igualdadeCartas(){
     if (primeiraCarta.innerHTML === segundaCarta.innerHTML){
         numeroCartas = numeroCartas - 2;
-        primeiraCarta.classList.remove("cartaNaoOk");
-        segundaCarta.classList.remove("cartaNaoOk");
+        primeiraCarta.classList.remove("cartaErrada");
+        segundaCarta.classList.remove("cartaErrada");
         primeiraCarta = null;
         segundaCarta = null;
         conferirFinalJogo();
@@ -72,7 +91,9 @@ function desvirarCarta(){
 
 function conferirFinalJogo (){
     if (numeroCartas === 0){
-        alert (`Você ganhou em ${numeroJogadas} jogadas!!!!`);
+        alert (`Você ganhou em ${numeroJogadas} jogadas!!!! Tempo de jogo: ${contadorMinutos}m ${contadorSegundos}s`);
+        clearInterval(interval);
+
     }
 }
 
